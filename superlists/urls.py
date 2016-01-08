8f3731319bef62833c8b0ffbbe2f925fd53a7f13
Yong Cheng Toh <yongchengtoh@skysgdmac024.local>
2016-01-08 16:04:15 +0800

    Model for list Items and associated migrations

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
-import lists
+
+from lists import views
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
-    url(r'^$', lists.views.home_page, name="home")
+    url(r'^$', views.home_page, name="home")
 ]
